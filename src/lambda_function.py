1de6270f975457d50d22b4ccdf17ca052cc09f07
ngel rojas <aomarrojasp@gmail.com>
2024-07-27 17:02:08 -0300

    remove: isort removed

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,30 +1,28 @@
-import json
+# import json
+
 from flask import Flask, jsonify, request
 from werkzeug.middleware.proxy_fix import ProxyFix
-from werkzeug.wrappers import Request, Response
+
+# from werkzeug.wrappers import Request, Response
 
 app = Flask(__name__)
 
-# Apply ProxyFix to handle headers correctly in AWS Lambda
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
-@app.route('/api/send-document', methods=['GET'])
+@app.route("/api/send-document", methods=["GET"])
 def send_document():
-    # Simulate an event and context
     event = {
-        'httpMethod': 'GET',
-        'path': '/send-document',
-        'queryStringParameters': request.args,
-        'headers': dict(request.headers),
-        'body': None
+        "httpMethod": "GET",
+        "path": "/send-document",
+        "queryStringParameters": request.args,
+        "headers": dict(request.headers),
+        "body": None,
     }
     context = {}
 
-    # Call the handler function
     response = handler(event, context)
 
-    # Return the response from the handler
     return jsonify(response)
 
 
